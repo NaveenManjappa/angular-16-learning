743f72eb6504129de732a429f4849c8dff2bedf8
Naveen Manjappa <NManjappa@manh.com>
2024-07-11 07:59:29 +0100

    Reading and setting query strings

--- 10-angular-router/src/app/courses/courses.component.ts
+++ 10-angular-router/src/app/courses/courses.component.ts
@@ -1,13 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Course } from '../Models/course';
 import { CourseService } from '../Services/course.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
   coursesService = inject(CourseService);
   AllCourses: Course[] = this.coursesService.courses;
+  searchString:string;
+  activatedRoute: ActivatedRoute=inject(ActivatedRoute);
+
+  ngOnInit(){
+    //this.searchString=this.activatedRoute.snapshot.queryParams['search'];
+    //this.searchString=this.activatedRoute.snapshot.queryParamMap.get('search');
+
+   this.activatedRoute.queryParamMap.subscribe(res=>{
+    this.searchString=res.get('search');
+
+    console.log(this.searchString);
+
+    if(this.searchString ===undefined || this.searchString==='' || this.searchString === null){
+      this.AllCourses=this.coursesService.courses;
+    } else {
+      this.AllCourses=this.coursesService.courses.filter(x=>x.title.toLowerCase().includes(this.searchString.toLowerCase()));
+    }
+    
+    });
+
+    
+
+  }
 }
