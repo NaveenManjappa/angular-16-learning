ac4b385ea15869a2dbe4f8e6de39c2da70f7f4f2
Naveen Manjappa <NManjappa@manh.com>
2024-07-17 17:52:46 +0100

    Dynamic components - Ang16 project

--- 17-dynamic-components/src/app/app.component.spec.ts
+++ 17-dynamic-components/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dynamic-components'`, () => {
+  it(`should have as title '17-dynamic-components'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dynamic-components');
+    expect(app.title).toEqual('17-dynamic-components');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dynamic-components app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('17-dynamic-components app is running!');
   });
 });
--- 17-dynamic-components/src/app/app.module.ts
+++ 17-dynamic-components/src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ConfirmDeleteComponent } from './users/confirm-delete/confirm-delete.component';
-import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ConfirmDeleteComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
