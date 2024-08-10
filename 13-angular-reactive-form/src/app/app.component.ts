90669af2e4685bf8f95cacb307f0d356f4cdee05
Naveen Manjappa <NManjappa@manh.com>
2024-08-10 07:11:34 +0100

    Creating a simple reactive form

--- 13-angular-reactive-form/src/app/app.component.ts
+++ 13-angular-reactive-form/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'template-driven-form';
+
+  reactiveForm:FormGroup;
+
+  ngOnInit(): void {
+    this.reactiveForm=new FormGroup({
+      firstName: new FormControl(null),
+      lastName:new FormControl(null),
+      email:new FormControl(null),
+      username:new FormControl(null),
+      dob:new FormControl(null),
+      gender:new FormControl('male'),
+      street:new FormControl(null),
+      country:new FormControl('India'),
+      city:new FormControl(null),
+      region:new FormControl(null),
+      postal:new FormControl(null),
+      skills:new FormControl(null)
+    });
+  }
+
+  OnFormSubmitted() {
+    console.log(this.reactiveForm);
+  }
 } 
