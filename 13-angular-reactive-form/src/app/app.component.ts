582b38afb0bf31a3b903002da0f8682b2d889201
Naveen Manjappa <NManjappa@manh.com>
2024-08-18 08:00:25 +0100

    Adding form controls dynamically

--- 13-angular-reactive-form/src/app/app.component.ts
+++ 13-angular-reactive-form/src/app/app.component.ts
@@ -27,14 +27,21 @@ export class AppComponent implements OnInit{
         postal:new FormControl(null,Validators.required)
       }),      
       skills:new FormArray([
-        new FormControl(null,Validators.required),
-        new FormControl(null,Validators.required),
-        new FormControl(null,Validators.required)
+        new FormControl(null,Validators.required)        
       ])
     });
   }
 
   OnFormSubmitted() {
     console.log(this.reactiveForm);
+  }
+
+  AddSkills(){
+    (<FormArray>this.reactiveForm.get('skills')).push(new FormControl(null,Validators.required));
+  }
+
+  DeleteSkill(index){
+    const controls=<FormArray>this.reactiveForm.get('skills');
+    controls.removeAt(index);
   }
 } 
