f6447d2d4bf90155edec7a147109833cd9548829
Naveen Manjappa <NManjappa@manh.com>
2024-09-23 10:59:53 +0100

    Using HTTP Get method

--- 14-angular-http-client/src/app/dashboard/dashboard.component.ts
+++ 14-angular-http-client/src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent{
+export class DashboardComponent implements OnInit {
+  
   showCreateTaskForm: boolean = false;
   
   http:HttpClient=inject(HttpClient);
+  url= 'https://angularhttpclient-4d0cd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json';
 
+  ngOnInit(): void {
+    this.FetchAllTasks();
+  }
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
   }
@@ -22,11 +27,11 @@ export class DashboardComponent{
 
   CreateTask(data: Task) {
     console.log(data);
-    const url= 'https://angularhttpclient-4d0cd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json';
+    
 
     const httpHeaders=new HttpHeaders({'warehouse':'MN'});
 
-      this.http.post<{name:string}>(url,data,{headers:httpHeaders}).subscribe({
+      this.http.post<{name:string}>(this.url,data,{headers:httpHeaders}).subscribe({
         next: response => {
           console.log(response);
         },
@@ -37,5 +42,13 @@ export class DashboardComponent{
           console.log('Requested got completed');
         }
       });
+  }
+
+  private FetchAllTasks() {
+    this.http.get<{[key:string]:Task}>(this.url).subscribe({
+      next:(res)=>{
+        console.log(res);
+      }
+    });
   }
 }
