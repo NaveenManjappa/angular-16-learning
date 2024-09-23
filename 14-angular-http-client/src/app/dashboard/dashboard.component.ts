dd6b4dd14df568491cc2aa291d1cb1ccbd6300a5
Naveen Manjappa <NManjappa@manh.com>
2024-09-23 07:40:44 +0100

    Making http post request

--- 14-angular-http-client/src/app/dashboard/dashboard.component.ts
+++ 14-angular-http-client/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/Task';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,8 @@ import { Task } from '../Model/Task';
 })
 export class DashboardComponent{
   showCreateTaskForm: boolean = false;
+  
+  http:HttpClient=inject(HttpClient);
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -19,5 +22,18 @@ export class DashboardComponent{
 
   CreateTask(data: Task) {
     console.log(data);
+    const url= 'https://angularhttpclient-4d0cd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json';
+
+      this.http.post(url,data).subscribe({
+        next: response => {
+          console.log(response);
+        },
+        error: err=>{
+          console.log(err);
+        },
+        complete: ()=>{
+          console.log('Requested got completed');
+        }
+      });
   }
 }
