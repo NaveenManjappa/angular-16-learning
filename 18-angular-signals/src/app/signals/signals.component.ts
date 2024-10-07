7ca9b5298c593d6c0b402a4fcb7e3d4b3cd2f8f7
Naveen Manjappa <NManjappa@manh.com>
2024-10-07 12:52:00 +0100

    Using mutate method on signals

--- 18-angular-signals/src/app/signals/signals.component.ts
+++ 18-angular-signals/src/app/signals/signals.component.ts
@@ -8,19 +8,22 @@ import { Component, DoCheck , signal} from '@angular/core';
 export class SignalsComponent implements DoCheck {
    counter = signal(0);//writable signal
 
-   message: string[] = [];
+   message= signal<string[]>([]);
 
    ngDoCheck(): void {
      console.log('Angular change detection called!');
    }
+
    increment(){
     //this.counter.set(this.counter()+1);
     this.counter.update(val => val+1);
-    this.message.push('Current counter value is: ' + this.counter());
+    //this.message.update(prevMsg => [...prevMsg,'The current value of counter is:'+this.counter()] );
+    this.message.mutate(prevMsg=>prevMsg.push('The current value of counter is:'+this.counter()));
    }
 
    decrement(){
     this.counter.update(val => val-1);
-    this.message.pop();
+    //this.message.pop();
+    this.message.mutate(prevMsg=>prevMsg.pop());
    }
 }
