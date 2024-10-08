37450d3f69500b3029b533ddfc1ed0e24136dc59
Naveen Manjappa <NManjappa@manh.com>
2024-10-08 17:35:48 +0100

    Creating a Standalone directive

--- 19-angular-standalone/src/app/app.module.ts
+++ 19-angular-standalone/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './home/detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent    
   ],
   imports: [
-    BrowserModule,    
-    DetailComponent
+    BrowserModule,        
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 19-angular-standalone/src/app/home/home.component.ts
+++ 19-angular-standalone/src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DetailComponent } from './detail/detail.component';
 
 @Component({
+  standalone:true,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  //imports:[DetailComponent] valid only for standalone component
+  imports:[DetailComponent] //valid only for standalone component
 })
 export class HomeComponent {
 
