3adccbb01db4d44c71dcec00ff6d6c364faab4e2
Naveen Manjappa <NManjappa@manh.com>
2024-10-08 17:25:37 +0100

    Converting to a standalone component

--- 19-angular-standalone/src/app/app.module.ts
+++ 19-angular-standalone/src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './home/detail/detail.component';
-import { HighlightDirective } from './shared/highlight.directive';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DetailComponent
-    
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    SharedModule
+    BrowserModule,    
+    DetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 19-angular-standalone/src/app/home/home.component.ts
+++ 19-angular-standalone/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DetailComponent } from './detail/detail.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  //imports:[DetailComponent] valid only for standalone component
 })
 export class HomeComponent {
 
