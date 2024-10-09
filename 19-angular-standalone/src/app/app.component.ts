0ba5741255aebaa1e704d66a5a3a45efae2dd4f3
Naveen Manjappa <NManjappa@manh.com>
2024-10-09 06:36:49 +0100

    Creating a root standalone component

--- 19-angular-standalone/src/app/app.component.ts
+++ 19-angular-standalone/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone:true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[HomeComponent]
 })
 export class AppComponent {
   title = '19-angular-standalone';
