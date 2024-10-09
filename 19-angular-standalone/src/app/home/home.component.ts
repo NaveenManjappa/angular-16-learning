4d1c8ef3607d9623f49a6d184c6dd62d30588f3d
Naveen Manjappa <NManjappa@manh.com>
2024-10-09 06:52:12 +0100

    Provind a service to standalone component

--- 19-angular-standalone/src/app/home/home.component.ts
+++ 19-angular-standalone/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DetailComponent } from './detail/detail.component';
+import { ActionService } from '../shared/action.service';
 
 @Component({
   standalone:true,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports:[DetailComponent] //valid only for standalone component
+  imports:[DetailComponent], //valid only for standalone component
+  providers:[ActionService]
 })
 export class HomeComponent {
 
--- 19-angular-standalone/src/main.ts
+++ 19-angular-standalone/src/main.ts
@@ -1,8 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { ActionService } from './app/shared/action.service';
 
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent,{
+  providers:[ActionService] // equivalent to providing service at the root module level
+});
